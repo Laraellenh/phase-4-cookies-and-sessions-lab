fe883e630d3e79897c51bab29fedbe84db3215c5
Lara <laraellenh@gmail.com>
2022-03-23 12:11:48 -0400

    completed the sessions and cookies stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    a = Article.find(params[:id])
+    session[:page_views] ||= 0
+    session[:page_views]  +=1
+    if session[:page_views] <=3  
+      render json: a
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+      end
   end
 
   private
 
   def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+    render json: { error: "Article not found" }, status: 401
   end
 
 end
